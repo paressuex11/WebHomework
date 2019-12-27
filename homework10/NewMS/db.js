1c2d72d85c4d7d394bfac7f71261046cfcff9505
paressuex2001 <1138514846@qq.com>
2019-12-27 21:44:19 +0800

    0

--- homework10/NewMS/db.js
+++ homework10/NewMS/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/testdatabase", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/new_MS_db", { useNewUrlParser: true, useUnifiedTopology: true })
 
 var conn = mongoose.connection
 
